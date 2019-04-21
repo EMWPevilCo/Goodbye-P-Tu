ca94d07ef97600d687435c50749dd52d7f745d24
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2019-04-21 07:57:31 +0700

    Auto stash before merge of "master" and "origin/master"

--- inject.js
+++ inject.js
@@ -1,74 +1,67 @@
-(function() {
-  var media = new Audio(chrome.runtime.getURL("/assets/song.mp3"));
-  var playPromise = media.play();
+(function () {
+	var media = new Audio(chrome.runtime.getURL("assets/song.mp3"));
+	const playPromise = media.play();
+	if (playPromise !== null){
+    playPromise.catch(() => { media.play(); })
+}
 
-  if (playPromise !== undefined) {
-    playPromise
-      .then(_ => {
-        media.play();
-        console.log("Music!");
-      })
-      .catch(error => {
-        media.pause();
-        console.log("Music Error");
-      });
-  }
 
-  //Below this line is the injected script
-  //you can edit easily as an usual javascript on website
 
-  // just place a div at top right
-  //var data = []
+	//Below this line is the injected script
+	//you can edit easily as an usual javascript on website
 
-  /*alert('inserted self... giggity');
-	var cSite = window.location.href;
-	var img = document.getElementsByTagName("img, a, i");
-	var data = []
+	// just place a div at top right
+	//var data = []
 
-	function checkURL(currentSite) {
-		var hostName = window.location.hostname.split('.');
-		currentSite = currentSite.split('.');
+	/*alert('inserted self... giggity');
+	  var cSite = window.location.href;
+	  var img = document.getElementsByTagName("img, a, i");
+	  var data = []
+  
+	  function checkURL(currentSite) {
+		  var hostName = window.location.hostname.split('.');
+		  currentSite = currentSite.split('.');
+  
+		  for (var x in currentSite) {
+			  if (currentSite[x] == "") {
+				  currentSite.splice(x, 1);
+			  }
+		  }
+		  var j = hostName.length - currentSite.length
+		  for (var i in currentSite) {
+			  if (!hostName[j] || hostName[j].toLowerCase() != currentSite[i].toLowerCase()) {
+				  return false;
+			  }
+			  j++;
+		  }
+		  return true;
+	  }
+  
+	  if (checkURL("facebook.com") === true) {
+		  alert("Yay it's facebook")
+		  for (var i = 0; i < img.length; i++) {
+			  data.push(img[i].src);
+		  }
+		  console.log('Facebook:' + data[1])
+	  }
+  
+	  if (checkURL("instagram.com") === true) {
+		  alert(" Yay it's instagram")
+		  for (var i = 0; i < img.length; i++) {
+			  data.push(img[i].src);
+		  }
+		  console.log('Instagram:' + data[1])
+	  }
+	  //not tested yet
+	  if (checkURL("twitter.com") === true) {
+		  alert("Yay it's twitter")
+		  for (var i = 0; i < img.length; i++) {
+			  data.push(img[i].src);
+		  }
+		  console.log('twitter:' + data[1])
+	  }
+	  */
 
-		for (var x in currentSite) {
-			if (currentSite[x] == "") {
-				currentSite.splice(x, 1);
-			}
-		}
-		var j = hostName.length - currentSite.length
-		for (var i in currentSite) {
-			if (!hostName[j] || hostName[j].toLowerCase() != currentSite[i].toLowerCase()) {
-				return false;
-			}
-			j++;
-		}
-		return true;
-	}
-
-	if (checkURL("facebook.com") === true) {
-		alert("Yay it's facebook")
-		for (var i = 0; i < img.length; i++) {
-			data.push(img[i].src);
-		}
-		console.log('Facebook:' + data[1])
-	}
-
-	if (checkURL("instagram.com") === true) {
-		alert(" Yay it's instagram")
-		for (var i = 0; i < img.length; i++) {
-			data.push(img[i].src);
-		}
-		console.log('Instagram:' + data[1])
-	}
-	//not tested yet
-	if (checkURL("twitter.com") === true) {
-		alert("Yay it's twitter")
-		for (var i = 0; i < img.length; i++) {
-			data.push(img[i].src);
-		}
-		console.log('twitter:' + data[1])
-	}
-	*/
-
-  //test script
-  //document.querySelectorAll("img")[1].getAttribute("src")
+	//test script
+	//document.querySelectorAll("img")[1].getAttribute("src")
 })();
